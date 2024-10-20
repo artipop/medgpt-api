08721a4a8c642b4e3b5f69ad38f0fcc16c0521dd
engineer-alekseev <engineer-alekseev@ya.com>
2024-10-20 21:11:58 +0700

    fix issue with incorrect relation in user table

--- src/users/models/models.py
+++ src/users/models/models.py
@@ -8,18 +8,18 @@ from sqlalchemy.orm import relationship, Mapped, mapped_column
 from database import Base, AbstractRepository
 
 
-class User(Base):
-    __tablename__ = 'users'
-    id: Mapped[UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
-    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(TIMESTAMP, default=datetime.now)
+# class User(Base):
+#     __tablename__ = 'users'
+#     id: Mapped[UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
+#     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+#     created_at: Mapped[datetime] = mapped_column(TIMESTAMP, default=datetime.now)
 
-    refresh_tokens = relationship("RefreshTokens", back_populates="user")
-
-
+#     refresh_tokens = relationship("RefreshTokens", back_populates="user")
 
 
 
 
-class UserRepository(AbstractRepository):
-    model =  User
+
+
+# class UserRepository(AbstractRepository):
+#     model =  User
